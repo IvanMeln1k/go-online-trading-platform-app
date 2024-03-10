0e7e8fe46e8705a6fd6c1a6bb10f6858a8492838
Ivan Melnik <IvanMelnikV@ya.ru>
2024-03-10 09:08:19 +0300

    repository/users: add getbyid, getbyemail, getbyusername, update

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -14,6 +14,9 @@ const (
 type Users interface {
 	Create(ctx context.Context, user domain.User) (int, error)
 	GetById(ctx context.Context, id int) (domain.User, error)
+	GetByEmail(ctx context.Context, email string) (domain.User, error)
+	GetByUserName(ctx context.Context, username string) (domain.User, error)
+	Update(ctx context.Context, id int, data domain.UserUpdate) (domain.User, error)
 }
 
 type Repository struct {
--- internal/repository/users.go
+++ internal/repository/users.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/IvanMeln1k/go-online-trading-platform-app/domain"
 	"github.com/jmoiron/sqlx"
@@ -38,11 +39,65 @@ func (r *UsersRepository) Create(ctx context.Context, user domain.User) (int, er
 	return id, nil
 }
 
-func (r *UsersRepository) GetById(ctx context.Context, id int) (domain.User, error) {
+func (r *UsersRepository) get(ctx context.Context, key string,
+	value interface{}) (domain.User, error) {
 	var user domain.User
 
-	query := fmt.Sprintf(`SELECT * FROM %s WHERE id = $1`, usersTable)
-	err := r.db.Get(&user, query, id)
+	query := fmt.Sprintf(`SELECT * FROM %s WHERE %s = $1`, usersTable, key)
+	err := r.db.Get(&user, query, value)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return user, ErrUserNotFound
+		}
+		return user, ErrInternal
+	}
+
+	return user, nil
+}
+
+func (r *UsersRepository) GetById(ctx context.Context, id int) (domain.User, error) {
+	return r.get(ctx, "id", id)
+}
+
+func (r *UsersRepository) GetByEmail(ctx context.Context, email string) (domain.User, error) {
+	return r.get(ctx, "email", email)
+}
+
+func (r *UsersRepository) GetByUserName(ctx context.Context, username string) (domain.User, error) {
+	return r.get(ctx, "username", username)
+}
+
+func (r *UsersRepository) Update(ctx context.Context, id int, data domain.UserUpdate) (domain.User, error) {
+	var user domain.User
+
+	var names []string
+	var values []interface{}
+	argId := 1
+
+	addProp := func(name string, value interface{}) {
+		names = append(names, fmt.Sprintf("%s = $%d", name, argId))
+		values = append(values, value)
+		argId++
+	}
+
+	if data.Email != nil {
+		addProp("email", *data.Email)
+	}
+	if data.Name != nil {
+		addProp("name", *data.Email)
+	}
+	if data.Password != nil {
+		addProp("password", *data.Password)
+	}
+	if data.Username != nil {
+		addProp("username", *data.Username)
+	}
+
+	setQuery := strings.Join(names, ", ")
+	values = append(values, id)
+	query := fmt.Sprintf(`UPDATE %s u SET %s WHERE id = $%d RETURNING u.*`,
+		usersTable, setQuery, argId+1)
+	err := r.db.Get(&user, query, values...)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return user, ErrUserNotFound
