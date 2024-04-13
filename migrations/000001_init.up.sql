6a0092cb61113388c19b33cc93e35d5b447bf601
IvanMeln1k <142226319+IvanMeln1k@users.noreply.github.com>
2024-04-13 22:42:05 +0300

    Revert "Карты "

--- migrations/000001_init.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(1024) NOT NULL,
-    name VARCHAR(1024) NOT NULL,
-    email VARCHAR(1024) NOT NULL,
-    hash_password VARCHAR(1024) NOT NULL,
-    email_verified BOOL DEFAULT false
-);
--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -1,10 +1,10 @@
 package database
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq"
 )
 
 type PostgresConfig struct {
@@ -16,12 +16,16 @@ type PostgresConfig struct {
 	SSLMode  string
 }
 
+var (
+	ErrConnPostgres = errors.New("error connect to postgres db")
+)
+
 func NewPostgresDB(cfg PostgresConfig) (*sqlx.DB, error) {
 	strConn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode)
 	conn, err := sqlx.Connect("postgres", strConn)
 	if err != nil {
-		return nil, err
+		return nil, ErrConnPostgres
 	}
 	return conn, nil
 }
